a32ddc4adcd3d461997f18870839541cfbe241e5
giangnv <giang.nguyen@hyperlogy.com>
2019-10-15 15:37:52 +0700

    request getmapping findbyId

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,12 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.model.Employee;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
+import javax.xml.ws.Response;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -21,5 +21,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.findAll();
+    }
+
+    @RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long id) {
+        Employee employee = employeeService.findByIdEmployee(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 }
