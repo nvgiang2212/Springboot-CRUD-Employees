cf3e3f9afa5f95c537798fa2f30c20e631321e4c
giangnv <giang.nguyen@hyperlogy.com>
2019-10-15 15:31:06 +0700

    request getmapping findall

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.constraints.Max;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.findAll();
+    }
 }
