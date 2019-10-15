32cf380416cb183288e28786c1da580b4257db92
giangnv <giang.nguyen@hyperlogy.com>
2019-10-15 14:45:12 +0700

    first commit

---
+++ src/main/java/com/example/demo/SpringbootDemoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoCrudApplication.class, args);
+    }
+
+}
