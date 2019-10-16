ef21bbb1f0a5de69364616f6ef549658ef5effad
giangnv <giang.nguyen@hyperlogy.com>
2019-10-16 14:59:35 +0700

    changes repository employees

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findAllById(Long id);
+    Optional<Employee> findAllByIdContaining(String employee_name);
+    List<Employee> findAllById (Long id);
 }
