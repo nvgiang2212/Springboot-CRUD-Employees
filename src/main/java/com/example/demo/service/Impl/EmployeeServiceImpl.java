92dd68a71839607d2e7412d2ffab11e1cb9eb57b
giangnv <giang.nguyen@hyperlogy.com>
2019-10-16 15:09:24 +0700

    changesssss service employees Impl

--- src/main/java/com/example/demo/service/Impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/EmployeeServiceImpl.java
@@ -1,4 +1,40 @@
 package com.example.demo.service.Impl;
 
-public class EmployeeServiceImpl {
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Optional<Employee> findById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    @Override
+    public Employee findByIdEmployee(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void delete(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
