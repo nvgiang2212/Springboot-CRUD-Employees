2fed98d1ef3c4fdd1a8f27098c5e8b906efd4086
giangnv <giang.nguyen@hyperlogy.com>
2019-10-16 14:52:44 +0700

    changes service employees

---
+++ src/main/java/com/example/demo/repository/Impl/EmployeeRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.example.demo.repository.Impl;
+
+public class EmployeeRepositoryImpl {
+}
--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -4,12 +4,19 @@ import com.example.demo.model.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface EmployeeService {
+
     List<Employee> findAll();
-    List<Employee> findAllById(Long id);
+
+    Optional<Employee> findById(Long id);
+
     Employee findByIdEmployee(Long id);
+
     void save(Employee employee);
+
     void delete(Long id);
+
 }
